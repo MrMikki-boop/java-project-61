c4d105b2c1f9aa3b6b466eec84960dd8680f06b0
maks <nickitkacozloff@yandex.ru>
2023-07-20 18:41:55 +0300

    Add GCD game and update App class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import hexlet.code.games.CalcGame;
 import hexlet.code.games.EvenGame;
+import hexlet.code.games.GcdGame;
 
 import java.util.Scanner;
 
@@ -16,6 +17,7 @@ public class App {
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         int choice = scanner.nextInt();
@@ -30,6 +32,9 @@ public class App {
                 break;
             case 3:
                 CalcGame.startGame();
+                break;
+            case 4:
+                GcdGame.startGame();
                 break;
             case 0:
                 System.out.println("Goodbye!");
---
+++ app/src/main/java/hexlet/code/games/GcdGame.java
@@ -0,0 +1,33 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+import java.util.Random;
+
+public class GcdGame {
+    private static final int MAX_QUESTIONS = 3;
+    private static final int MAX_NUMBER = 100;
+
+    public static void startGame() {
+        Engine.playGame("Find the greatest common divisor of given numbers.", MAX_QUESTIONS, GcdGame::generateQuestionAndAnswer);
+    }
+
+    private static String generateQuestionAndAnswer() {
+        Random random = new Random();
+        int number1 = random.nextInt(MAX_NUMBER) + 1;
+        int number2 = random.nextInt(MAX_NUMBER) + 1;
+
+        String question = number1 + " " + number2;
+        String answer = String.valueOf(calculateGcd(number1, number2));
+        return question + "\n" + answer;
+    }
+
+    private static int calculateGcd(int number1, int number2) {
+        while (number2 != 0) {
+            int temp = number1 % number2;
+            number1 = number2;
+            number2 = temp;
+        }
+        return Math.abs(number1);
+    }
+}
