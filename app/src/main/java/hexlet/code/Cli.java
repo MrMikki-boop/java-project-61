e7a9c2721ab7192337ba675f655dbe6097db5a90
maks <nickitkacozloff@yandex.ru>
2023-07-20 17:47:25 +0300

    Add checkstyle plugin and fix code style issues

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Cli {
     public static void greetUser() {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String userName = scanner.next();
@@ -13,4 +14,3 @@ public class Cli {
         scanner.close();
     }
 }
-
