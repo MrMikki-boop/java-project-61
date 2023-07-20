b84432c453410376344625d1b55a3b1d634ac0d8
maks <nickitkacozloff@yandex.ru>
2023-07-20 19:05:32 +0300

    update ProgressionGame

--- app/src/main/java/hexlet/code/games/ProgressionGame.java
+++ app/src/main/java/hexlet/code/games/ProgressionGame.java
@@ -10,8 +10,6 @@ public class ProgressionGame {
     private static final int PROGRESSION_LENGTH_MAX = 10;
     private static final int PROGRESSION_MIN_STEP = 1;
     private static final int PROGRESSION_MAX_STEP = 10;
-    private static final int PROGRESSION_HIDDEN_INDEX_MIN = 0;
-    private static final int PROGRESSION_HIDDEN_INDEX_MAX = PROGRESSION_LENGTH_MAX - 1;
 
     public static void startGame() {
         Engine.playGame("What number is missing in the progression?", MAX_QUESTIONS,
@@ -20,11 +18,9 @@ public class ProgressionGame {
 
     private static String generateQuestionAndAnswer() {
         Random random = new Random();
-        int progressionLength = random.nextInt(PROGRESSION_LENGTH_MAX - PROGRESSION_LENGTH_MIN + 1)
-                + PROGRESSION_LENGTH_MIN;
+        int progressionLength = random.nextInt(PROGRESSION_LENGTH_MAX - PROGRESSION_LENGTH_MIN + 1) + PROGRESSION_LENGTH_MIN;
         int progressionStep = random.nextInt(PROGRESSION_MAX_STEP - PROGRESSION_MIN_STEP + 1) + PROGRESSION_MIN_STEP;
-        int hiddenIndex = random.nextInt(PROGRESSION_HIDDEN_INDEX_MAX - PROGRESSION_HIDDEN_INDEX_MIN + 1)
-                + PROGRESSION_HIDDEN_INDEX_MIN;
+        int hiddenIndex = random.nextInt(progressionLength);
 
         int[] progression = generateProgression(progressionLength, progressionStep);
         String question = formatProgression(progression, hiddenIndex);
