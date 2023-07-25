3374e5534a5cac31b190c124d3dd3b67745b8c0b
maks <nickitkacozloff@yandex.ru>
2023-07-25 18:34:39 +0300

    Refactor Engine and .code.games

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -3,16 +3,22 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Engine {
+    public static final int MAX_NUMBER = 100;
     public static void playGame(String[][] rounds, String question) {
-        String name = Cli.greetUser();
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+
         System.out.println(question);
 
         for (String[] round : rounds) {
             String questionText = round[0];
             String correctAnswer = round[1];
 
-            System.out.println("Question: " + questionText);
+            System.out.println(questionText);
             System.out.print("Your answer: ");
             String userAnswer = scanner.next();
 
