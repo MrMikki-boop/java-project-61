fe7f6ae1286b57e7b3fd8e4cbda39125a436ef54
maks <nickitkacozloff@yandex.ru>
2023-07-25 08:35:49 +0300

    Fix magic numbers in CalcGameTes

--- app/src/test/java/hexlet/code/games/CalcGameTest.java
+++ app/src/test/java/hexlet/code/games/CalcGameTest.java
@@ -5,24 +5,30 @@ import org.junit.jupiter.api.Test;
 
 public class CalcGameTest {
 
+    private static final int NUMBER_1 = 2;
+    private static final int NUMBER_2 = 3;
+    private static final int NUMBER_3 = 5;
+    private static final int NUMBER_4 = 4;
+    private static final int NUMBER_5 = 20;
+
     @Test
     public void testGenerateQuestion() {
-        String question = CalcGame.generateQuestion(2, 3, '+');
+        String question = CalcGame.generateQuestion(NUMBER_1, NUMBER_2, '+');
         Assertions.assertEquals("2 + 3", question);
     }
 
     @Test
     public void testCalculateExpression() {
-        int result1 = CalcGame.calculateExpression(2, 3, '+');
-        Assertions.assertEquals(5, result1);
+        int result1 = CalcGame.calculateExpression(NUMBER_1, NUMBER_2, '+');
+        Assertions.assertEquals(NUMBER_3, result1);
 
-        int result2 = CalcGame.calculateExpression(5, 2, '-');
-        Assertions.assertEquals(3, result2);
+        int result2 = CalcGame.calculateExpression(NUMBER_3, NUMBER_1, '-');
+        Assertions.assertEquals(NUMBER_2, result2);
 
-        int result3 = CalcGame.calculateExpression(4, 5, '*');
-        Assertions.assertEquals(20, result3);
+        int result3 = CalcGame.calculateExpression(NUMBER_4, NUMBER_3, '*');
+        Assertions.assertEquals(NUMBER_5, result3);
 
-        Assertions.assertThrows(IllegalStateException.class, () -> CalcGame.calculateExpression(2, 3, '/'));
+        Assertions.assertThrows(IllegalStateException.class, () -> CalcGame.calculateExpression(NUMBER_1, NUMBER_2, '/'));
     }
 
     @Test
